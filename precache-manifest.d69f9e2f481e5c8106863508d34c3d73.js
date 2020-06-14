a1c0a055beb5d8026dc9d73450df7c64401d4d12
elkarthi2000@gmail.com <elkarthi2000@gmail.com>
2020-06-14 21:17:29 +0800

    Updates

---
+++ precache-manifest.d69f9e2f481e5c8106863508d34c3d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294847d1fcf4e314233c5116d342f916",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "aba76b4f04f7b3a4549c",
+    "url": "/RecipeApp/static/css/main.43ffc5b2.chunk.css"
+  },
+  {
+    "revision": "1b837095736aea275afc",
+    "url": "/RecipeApp/static/js/2.9b43fe83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeApp/static/js/2.9b43fe83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba76b4f04f7b3a4549c",
+    "url": "/RecipeApp/static/js/main.55c44ad0.chunk.js"
+  },
+  {
+    "revision": "464c6751fba2d961a718",
+    "url": "/RecipeApp/static/js/runtime-main.f5de1909.js"
+  }
+]);
